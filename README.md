3393f2594a90fadedbb0f5a3fa39fcf7aff72571
즈눅 <admin@xnu.kr>
2016-11-02 15:49:45 +0900

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# K-SSVM for Node.js
+[준비된 한국형 언어 GNE](https://github.com/akic4op4/GNE)의 SoonSil Viseonsilse Machine 구현체입니다. 현존하는 대부분의 컴퓨터를 지원합니다.
+
+## 실행 방법
+
+```
+npm install --global
+ssvm
+```
+
+또는
+
+```
+node ./index.js
+```
---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+if(require('os').freemem()/(1024*1024*1024) < 486) {
+	console.error('하드웨어 경고: 램 없으면 니네 부모를 원망해. 램도 실력이야. 있는 우리 컴퓨터 가지고 감 놔라 배 놔라 하지 말고. 남의 욕하기 바쁘니 아무리 다른 것 한들 어디 성공하겠니? 모자란 애들 상대하기 더러워서 안 하는 거야')
+	process.exit(126)
+}
+
+// TODO: Intel® Xeon® Processor E7
+// TODO: 승마장
+// TODO: 독일 직통 광케이블
+console.error('아직 지원되지 않는 컴퓨터입니다.')
